7beeef4054f4474f1e35ebf62269017c4a39ba82
SixSense <33900575+SixSen@users.noreply.github.com>
2019-01-12 20:55:12 +0800

    完成了个人中心的基础搭建并完善了歌曲的购买与播放逻辑

--- app/home/views.py
+++ app/home/views.py
@@ -1,21 +1,21 @@
 from flask import Blueprint, render_template, redirect, flash, url_for, session, request, Response
 from werkzeug.security import generate_password_hash
 from . import home
-from app.home.forms import LoginForm, RegisterForm
-from app.models import User, Music, Board
-from app import db, app, rd
+from app.home.forms import LoginForm, RegisterForm, UserdetailForm, PwdForm
+from app.models import User, Music, Board, Buy, db
+# from app import db, app, rd
 import pymysql
 
 
+# pymysql的数据库连接
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='1232123', db='musicdb')
-# conn = pymysql.connect(host='39.106.214.230', port=3306, user='root', passwd='nucoj', db='musicdb')
-#@ home = Blueprint("home",__name__)
 
+# conn = pymysql.connect(host='39.106.214.230', port=3306, user='root', passwd='nucoj', db='musicdb')
+# @ home = Blueprint("home",__name__)
 # session.permantent = True
 
 
-
-
+# 根路由
 @home.route("/")
 def index():
     page_data = Board.query.filter(
@@ -24,15 +24,18 @@ def index():
     )
     return render_template("home/index.html", page_data=page_data)
 
+
+# 登陆后的欢迎主页
 @home.route("/welcome/")
 def welcome():
     page_data = Board.query.filter(
     ).order_by(
         Board.board_id
     )
-    return render_template("home/welcome.html", name = session.get('user'), vclass =session.get('vclass'), page_data=page_data)
+    return render_template("home/welcome.html", name=session.get('user'), vclass=session.get('vclass'), page_data=page_data)
 
-#登录
+
+# 登录
 @home.route("/login/", methods=["GET", "POST"])
 def login():
     form = LoginForm()
@@ -56,15 +59,88 @@ def login():
         return redirect(url_for("home.welcome"))
     return render_template("home/login.html", form=form)
 
-# 退出
+
+# 退出登录
 @home.route("/logout/")
 def logout():
     session.pop("user", None)
     session.pop("user_id", None)
     session.pop("vclass", None)
+    flash("您已成功登出！", "ok")
     return redirect(url_for('home.login'))
 
-# 播放
+
+# 个人中心
+@home.route("/user/", methods=["GET", "POST"])
+# @user_login_req
+def user():
+    form = UserdetailForm()
+    user = User.query.get(int(session["user_id"]))
+    if request.method == "GET":
+        form.name.data = user.name
+        form.email.data = user.email
+        form.phone.data = user.phone
+    if form.validate_on_submit():
+        data = form.data
+        name_count = User.query.filter_by(name=data["name"]).count()
+        if data["name"] != user.name and name_count == 1:
+            flash("昵称已经存在！", "err")
+            return redirect(url_for("home.user"))
+
+        email_count = User.query.filter_by(email=data["email"]).count()
+        if data["email"] != user.email and email_count == 1:
+            flash("邮箱已经存在！", "err")
+            return redirect(url_for("home.user"))
+
+        phone_count = User.query.filter_by(phone=data["phone"]).count()
+        if data["phone"] != user.phone and phone_count == 1:
+            flash("手机号码已经存在！", "err")
+            return redirect(url_for("home.user"))
+
+        user.name = data["name"]
+        user.email = data["email"]
+        user.phone = data["phone"]
+        db.session.add(user)
+        db.session.commit()
+        flash("修改成功！", "ok")
+        return redirect(url_for("home.user"))
+    return render_template("home/user.html", name=session.get('user'), form=form, user=user)
+
+# 密码修改
+@home.route("/pwd/", methods=["GET", "POST"])
+def pwd():
+    form = PwdForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User.query.filter_by(name=session["user"]).first()
+        if not user.check_pwd(data["old_pwd"]):
+            flash("旧密码错误！", "err")
+            return redirect(url_for('home.pwd'))
+        user.pwd = generate_password_hash(data["new_pwd"])
+        db.session.add(user)
+        db.session.commit()
+        flash("修改密码成功，请重新登录！", "ok")
+        return redirect(url_for('home.logout'))
+    return render_template("home/pwd.html",name=session.get('user'),  form=form)
+
+# 订阅会员
+@home.route("/sub/", methods=["GET", "POST"])
+def sub():
+    form = PwdForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User.query.filter_by(name=session["user"]).first()
+        if not user.check_pwd(data["old_pwd"]):
+            flash("旧密码错误！", "err")
+            return redirect(url_for('home.pwd'))
+        user.pwd = generate_password_hash(data["new_pwd"])
+        db.session.add(user)
+        db.session.commit()
+        flash("修改密码成功，请重新登录！", "ok")
+        return redirect(url_for('home.logout'))
+    return render_template("home/subscribe.html",name=session.get('user'),  form=form)
+
+# 播放音乐
 @home.route("/play/")
 def play():
     if session.get('user') is None:
@@ -98,6 +174,8 @@ def play():
             return render_template("home/msg.html")
     else:
         return render_template("home/play.html", name=session.get('user'), user=session.get('user_id'), id=musicd)
+
+
 # 注册
 @home.route("/register/", methods=["GET", "POST"])
 def register():
@@ -119,14 +197,50 @@ def register():
 # 购买
 @home.route("/buy")
 def buy():
-    return "buy"
+    musicd = int(request.args.get('id'))
+    user_id = session.get('user_id')
+    cursor = conn.cursor()
+    sql = "SELECT music_id FROM buy WHERE id = '%s' " % user_id
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    sql = "SELECT free FROM music WHERE music_id = '%s' " % musicd
+    cursor.execute(sql)
+    results0 = cursor.fetchall()
+    print(results0[0])
+    for k in results0:
+        if 1 == k[0]:
+            flash("此歌曲为免费歌曲，无需购买")
+            return render_template("home/msg.html")
+    for rol in results:
+        if musicd == rol[0]:
+            flash("您已经购买过此歌曲，请勿重复购买！")
+            return render_template("home/msg.html")
+    result = User.query.filter(User.id == session.get('user_id')).first()
+    uss = result.wallet
+    uss = uss - 2
+    # print(uss)
+    if uss < 0:
+        flash("账户余额不足，请充值后再试")
+    else:
+        sql = "UPDATE user SET wallet = '%s' WHERE id = '%s' " % (uss, user_id)
+        cursor.execute(sql)
+        conn.commit()
+        wallet = uss
+        buy = Buy(
+            id=session.get('user_id'),
+            music_id=musicd
+        )
+        db.session.add(buy)
+        db.session.commit()
+        flash("购买成功，账户扣除2元，当前余额%d元" % wallet)
+    return render_template("home/msg.html")
+
 
 # 搜索
 @home.route("/search")
 def search():
     key = request.args.get('key')
-    print("key:",key)
-
     count = Music.query.filter(
         Music.music_name.ilike('%' + key + '%')
     ).count()
@@ -137,8 +251,6 @@ def search():
         Music.listen.desc()
     )
     page_data.key = key
-    print((page_data))
-
     # conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='1232123', db='musicdb')
     # cursor = conn.cursor()
     # sql = "SELECT * FROM music WHERE music_name = '%s' " % key
@@ -146,5 +258,4 @@ def search():
     # cursor.execute(sql)
     # results = cursor.fetchall()
     # print(results)
-
-    return render_template("home/search.html", name = session.get('user'),key=key,count= count, page_data=page_data)
+    return render_template("home/search.html", name=session.get('user'), key=key, count=count, page_data=page_data)
